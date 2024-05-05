fb784dfae090bc372be5a541ee2f2dd1ac2dfccc
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-05 20:18:16 +0200

    Add question.js file to fetch from it question & answers

---
+++ JS/questions.js
@@ -0,0 +1,68 @@
+let questions = [
+  {
+    question:
+      "Which of the following is a valid type of function javascript supports?",
+    answers: [
+      "named function",
+      "anonymous function",
+      "Both of the above",
+      "None of the above",
+    ],
+    correctAnswer: "Both of the above",
+  },
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+    correctAnswer: "Hyper Text Markup Language",
+  },
+  {
+    question: "What does CSS stand for?",
+    answers: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+    correctAnswer: "Cascading Style Sheet",
+  },
+  {
+    question:
+      "Which built-in method returns the calling string value converted to upper case?",
+    answers: [
+      "toUpperCase()",
+      "toUpper()",
+      "changeCase(case)",
+      "None of the above",
+    ],
+    correctAnswer: "toUpperCase()",
+  },
+  {
+    question:
+      "Which of the following function of String object returns the calling string value converted to lower case?",
+    answers: ["toLowerCase()", "toString()", "toLower()", "None of the above"],
+    correctAnswer: "toLowerCase()",
+  },
+  {
+    question:
+      "Which of the following function of Array object joins all elements of an array into a string?",
+    answers: ["concat()", "join()", "pop()", "push()"],
+    correctAnswer: "join()",
+  },
+  {
+    question:
+      "Which of the following function of Array object sorts the elements of an array?",
+    answers: ["sort()", "reverse()", "concat()", "slice()"],
+    correctAnswer: "sort()",
+  },
+  {
+    question:
+      "Which of the following function of String object returns the calling string value?",
+    answers: ["slice()", "split()", "search()", "substr()"],
+    correctAnswer: "slice()",
+  },
+];
